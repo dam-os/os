242f7e1d56421cc18c822985139468bfe78a0518
Marius Nielsen <mawn@itu.dk>
2025-02-13 12:10:19 +0100

    i think it works

--- src/kernel.c
+++ src/kernel.c
@@ -1,25 +1,47 @@
-#include <stdint.h>
+
 #include <stddef.h>
- 
-unsigned char * uart = (unsigned char *)0x10000000; 
-void putchar(char c) {
-	*uart = c;
-	return;
+#include <stdint.h>
+#define SYSCON_ADDR 0x100000
+
+#define UART_BASE 0x10000000
+#define UART_DATA (*(volatile uint8_t *)(UART_BASE + 0x00))
+#define UART_LSR (*(volatile uint8_t *)(UART_BASE + 0x05))
+#define UART_LSR_RX_READY 0x01
+#define UART_LSR_TX_EMPTY 0x20
+
+char getchar(void) {
+  while (!(UART_LSR & UART_LSR_RX_READY))
+    ; // Wait until a character is available
+  return UART_DATA;
 }
- 
-void print(const char * str) {
-	while(*str != '\0') {
-		putchar(*str);
-		str++;
-	}
-	return;
+
+char putchar(char c) {
+  while (!(UART_LSR & UART_LSR_TX_EMPTY))
+    ; // Wait until UART is ready to transmit
+  UART_DATA = c;
+  return c;
 }
- 
+
+void print(const char *str) {
+  while (*str != '\0') {
+    putchar(*str);
+    str++;
+  }
+}
+
+void poweroff(void) {
+  print("Poweroff requested\r\n");
+  *(uint32_t *)SYSCON_ADDR = 0x5555;
+}
+
 void kmain(void) {
-	print("Hello world!\r\n");
-	while(1) {
-		// Read input from the UART
-		putchar(*uart);
-	}
-	return;
+  print("Hello world!\r\n");
+  while (1) {
+    // Read a single character from the UART
+    char c = getchar();
+    putchar(c); // Echo back to the terminal
+    if (c == 'a') {
+      poweroff();
+    }
+  }
 }
