16d02bc62de36d998bbc1af587d3816f0250aa22
davidfeldner <davidfeldner12@gmail.com>
2025-02-16 19:08:28 +0100

    Don't change param pointers

--- src/lib/memory.c
+++ src/lib/memory.c
@@ -1,15 +1,19 @@
+#include "memory.h"
 #include <stddef.h>
 
-void memset(char *p, char c, size_t n) {
+void memset(const char *dst, char c, size_t n) {
+  char *d = dst;
   while (n > 0) {
-    *p++ = c;
+    *d++ = c;
     n--;
   }
 }
 
-void memcpy(char *src, char *dst, size_t n) {
+void memcpy(const char *src, const char *dst, size_t n) {
+  char *s = src;
+  char *d = dst;
   while (n > 0) {
-    *dst++ = *src++;
+    *d++ = *s++;
     n--;
   }
 }
--- src/lib/memory.h
+++ src/lib/memory.h
@@ -1,5 +1,5 @@
 
 #include <stddef.h>
 
-void memset(char *p, char c, size_t n);
-void memcpy(char *src, char *dst, size_t n);
+void memset(const char *p, char c, size_t n);
+void memcpy(const char *src, const char *dst, size_t n);
