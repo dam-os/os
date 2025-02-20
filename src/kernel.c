fd2efb8175655caec1ff3beac8ca20eab2f38420
Marius Nielsen <mawn@itu.dk>
2025-02-20 16:13:08 +0100

    alloc in kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,28 @@
 #include <stddef.h>
 
-#include "lib/print.h"
-#include "lib/memory.h"
-#include "lib/string.h"
 #include "lib/device_tree.h"
-#include "lib/uart.h"
+#include "lib/memory.h"
+#include "lib/paging.h"
 #include "lib/print.h"
+#include "lib/string.h"
 #include "lib/system.h"
+#include "lib/uart.h"
 
 #define PRINT_SYS_INFO 0
 
 void kmain(void) {
   uintptr_t dtb_address;
-  __asm__ volatile ("mv %0, a1" : "=r" (dtb_address));
+  __asm__ volatile("mv %0, a1" : "=r"(dtb_address));
 
-  if (PRINT_SYS_INFO) read_fdt(dtb_address);
-  
+  if (PRINT_SYS_INFO)
+    read_fdt(dtb_address);
+
   print("Hello world!\r\n");
+
+  int *page = alloc_pages(5);
+  alloc_pages(3);
+  free_pages(page, 5);
+  alloc_pages(3);
 
   /* print format */
   cprintf("hello %d\n", 1234567);
@@ -34,7 +40,6 @@ void kmain(void) {
   cprintf("%d\n", 100);
   cprintf("%d\n", 101);
 
-  
   PANIC("uh oh spaghettios %d", 5);
   print("we will never print this");
 }
