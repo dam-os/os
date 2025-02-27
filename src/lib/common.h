57d45de3fc4323cfeab8794fcd01ab816d3ba0ef
TheColorman <github@colorman.me>
2025-02-27 12:02:55 +0100

    Update common

--- src/lib/common.h
+++ src/lib/common.h
@@ -1,7 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
 typedef int uint32_t;
 typedef long long uint64_t;
+typedef long double uint128_t;
 
 #endif // !COMMON_H
