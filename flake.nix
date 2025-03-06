9f4a22da5abb01135ebadc557c84244ea0343990
TheColorman <github@colorman.me>
2025-03-06 12:12:23 +0100

    Add pwndbg to flake

--- flake.nix
+++ flake.nix
@@ -4,9 +4,17 @@
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    pwndbg = {
+      url = "github:pwndbg/pwndbg";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+    pwndbg,
+  }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells.x86_64-linux = {
@@ -14,30 +22,31 @@
       os = pkgs.mkShellNoCC {
         ENVIRONMENT_VARIABLE = "value";
 
-        packages = with pkgs; [
-           coreboot-toolchain.riscv
-           qemu
-           clang-tools_19 
-        ];
+        packages = with pkgs;
+          [
+            coreboot-toolchain.riscv
+            qemu
+            clang-tools_19
+          ]
+          ++ [
+            pwndbg.packages.x86_64-linux.default
+          ];
         shellHook = "echo 'Happy hacking'";
       };
     };
 
     packages.x86_64-linux = {
-      damos = pkgs.callPackage ./nix/package.nix { src = ./.; };
-      default = let
-        inherit (pkgs) qemu;
-        inherit (self.packages.x86_64-linux) damos;
-      in pkgs.writeShellApplication {
+      damos = pkgs.callPackage ./nix/package.nix {src = ./.;};
+      default = pkgs.writeShellApplication {
         name = "damos-vm";
         text = ''
-          ${qemu}/bin/qemu-system-riscv64 \
-            -machine virt \
-            -bios none \
-            -drive id=drive0,file=file.txt,format=raw,if=none \
-            -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
-            -kernel ${damos}/kernel.elf \
-            -serial mon:stdio
+          make run
+        '';
+      };
+      debug = pkgs.writeShellApplication {
+        name = "damos-vm";
+        text = ''
+          make debug
         '';
       };
       vm = self.packages.x86_64-linux.default;
