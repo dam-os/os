bf46a878f5bdb9b9690c0aec5808f8a4d00a87ab
Marius Nielsen <mawn@itu.dk>
2025-03-13 14:09:19 +0100

    go back to yes

--- src/lib/pci.h
+++ src/lib/pci.h
@@ -1,5 +1,3 @@
-uint32_t pci_read_word(uint8_t bus, uint8_t device, uint8_t function,
-                       uint8_t offset);
-void pci_write_word(uint8_t bus, uint8_t device, uint8_t function,
-                    uint8_t offset, uint32_t value);
+uint32_t pci_read_word(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset);
+void pci_write_word(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset, uint32_t value);
 void enumerate_pci();
