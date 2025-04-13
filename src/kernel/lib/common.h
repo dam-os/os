3045566e1e4069b7a26238fd1c5f013d735c6348
TheColorman <github@colorman.me>
2025-04-13 11:54:29 +0200

    fix(common): make uint64_t truly unsigned

--- src/kernel/lib/common.h
+++ src/kernel/lib/common.h
@@ -4,7 +4,7 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef int uint32_t;
-typedef long long uint64_t;
+typedef unsigned long long uint64_t;
 typedef long double uint128_t;
 typedef uint64_t uintptr_t;
 typedef uint64_t size_t;
