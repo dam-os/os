ff99964c31bcaf663b955c9c67bd7e9e1a21c3e1
davidfeldner <davidfeldner12@gmail.com>
2025-04-24 13:56:27 +0200

    Make exception stack big as tmp fix

--- src/kernel/lib/process.h
+++ src/kernel/lib/process.h
@@ -35,7 +35,7 @@ struct proc {
   proc_registers_t reg; // 8
   // NOTE: Pointers are 64-bit, but the stack is 8-bit aligned!
   uint8_t stack[4096]; // 128
-  uint8_t exception_stack[4096]; // Used to store exception data. Always in mscratch when process is running
+  uint8_t exception_stack[4096*1000]; // Used to store exception data. Always in mscratch when process is running
   uint64_t *page_table;
 };
 typedef struct proc proc_t;
