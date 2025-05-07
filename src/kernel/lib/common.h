18ba6ad763ad42029dfbe3820054248f33b05a9f
Colorman <foss@colorman.me>
2025-05-07 19:21:53 +0200

    feat(common): add signed values

--- src/kernel/lib/common.h
+++ src/kernel/lib/common.h
@@ -3,9 +3,13 @@
 
 // === Types ===
 
+typedef signed char s8;
 typedef unsigned char u8;
+typedef signed short int s16;
 typedef unsigned short int u16;
+typedef signed int s32;
 typedef unsigned int u32;
+typedef signed long int s64;
 typedef unsigned long int u64;
 typedef unsigned long int uptr;
 
