9ab345d0bbaca10a773370e36c5e369c5cc1df95
Colorman <foss@colorman.me>
2025-05-13 22:20:31 +0200

    feat(blog): re-work create-report script so dependencies are not in the build folder

--- blog/create-report.sh
+++ blog/create-report.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-mkdir -p pandoc_build
-cd pandoc_build
+WORKDIR=pandoc_build
 
-wget -O DAM-OS.md "https://hackmd.io/Fk70BI6qS-mr3AI3BT8CfQ/download"
+wget -O ${WORKDIR}/panDAM-OS.md "https://hackmd.io/Fk70BI6qS-mr3AI3BT8CfQ/download"
 
-pandoc -f markdown-implicit_figures DAM-OS.md \
-  -o out.pdf \
-  --csl ../ieee.csl \
+pandoc -f markdown-implicit_figures ${WORKDIR}/DAM-OS.md \
+  -o ${WORKDIR}/out.pdf \
+  --csl ieee.csl \
   --citeproc \
   --number-sections \
   --pdf-engine=pdflatex
