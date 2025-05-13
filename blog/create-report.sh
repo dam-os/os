3691fcdcddc8ea50380d5d499834ff4ead28b273
davidfeldner <davidfeldner12@gmail.com>
2025-05-13 23:12:06 +0200

    Use downloaded file in create report

--- blog/create-report.sh
+++ blog/create-report.sh
@@ -4,7 +4,7 @@ WORKDIR=pandoc_build
 
 wget -O ${WORKDIR}/panDAM-OS.md "https://hackmd.io/Fk70BI6qS-mr3AI3BT8CfQ/download"
 
-pandoc -f markdown-implicit_figures ${WORKDIR}/DAM-OS.md \
+pandoc -f markdown-implicit_figures ${WORKDIR}/panDAM-OS.md \
   -o ${WORKDIR}/out.pdf \
   --csl ieee.csl \
   --citeproc \
